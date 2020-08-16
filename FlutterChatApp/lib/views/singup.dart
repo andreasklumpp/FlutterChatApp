2ee5f5ae080e9e6091a5f5123abaed6c04ec99ba
andreasklumpp <ak272@hdm-stuttgart.de>
2020-08-16 16:55:11 +0200

    Creating Screens & Connect to Firebase

---
+++ FlutterChatApp/lib/views/singup.dart
@@ -0,0 +1,124 @@
+import 'package:FlutterChatApp/helper/theme.dart';
+import 'package:FlutterChatApp/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            Spacer(),
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    style: simpleTextStyle(),
+                    decoration: textFieldInputDecoration("username"),
+                  ),
+                  TextFormField(
+                    style: simpleTextStyle(),
+                    decoration: textFieldInputDecoration("email"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    validator: (val) {
+                      return val.length > 6
+                          ? null
+                          : "Enter Password 6+ characters";
+                    },
+                    style: simpleTextStyle(),
+                    decoration: textFieldInputDecoration("password"),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Text(
+                      "Forgot Password?",
+                      style: simpleTextStyle(),
+                    )),
+              ],
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            GestureDetector(
+              onTap: () {
+//TODO
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    gradient: LinearGradient(
+                      colors: [
+                        const Color(0xff007EF4),
+                        const Color(0xff2A75BC)
+                      ],
+                    )),
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  "Sign In",
+                  style: biggerTextStyle(),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 16),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30), color: Colors.white),
+              width: MediaQuery.of(context).size.width,
+              child: Text(
+                "Sign In with Google",
+                style: TextStyle(fontSize: 17, color: CustomTheme.textColor),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have account? ",
+                  style: simpleTextStyle(),
+                ),
+                Text(
+                  "Register now",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      decoration: TextDecoration.underline),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
